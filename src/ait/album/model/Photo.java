fe03fba52fed29087d11dea1d7cb0877526f9539
Maxim Nidens <almeranissan6@gmail.com>
2023-11-03 10:10:28 +0100

    update

---
+++ src/ait/album/model/Photo.java
@@ -0,0 +1,80 @@
+package ait.album.model;
+
+import java.time.LocalDateTime;
+
+public class Photo {
+    private int albumId;
+    private int photoId;
+    private String title;
+    private String url;
+    private LocalDateTime date;
+
+    public Photo(int albumId, int photoId, String title, String url, LocalDateTime date) {
+        this.albumId = albumId;
+        this.photoId = photoId;
+        this.title = title;
+        this.url = url;
+        this.date = date;
+    }
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public int getPhotoId() {
+        return photoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "albumId=" + albumId +
+                ", photoId=" + photoId +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Photo photo = (Photo) o;
+
+        if (albumId != photo.albumId) return false;
+        return photoId == photo.photoId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = albumId;
+        result = 31 * result + photoId;
+        return result;
+    }
+}
